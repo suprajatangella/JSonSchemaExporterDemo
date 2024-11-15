0f9125d04a419741eee13230976050428855bfe3
Supraja Tangella <supraja.tangella@gmail.com>
2024-11-15 17:04:12 +0000

    Add project files.

---
+++ JSonSchemaExporterDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NJsonSchema;
+using System.IO;
+//using Newtonsoft.Json.Schema;
+
+namespace JSonSchemaExporterDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var schema = JsonSchema.FromType<Product>();
+
+            var prodSchema = schema.ToJson();
+
+            File.WriteAllText("ProductSchema.json", prodSchema);
+
+            Console.WriteLine(prodSchema);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ JSonSchemaExporterDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JSonSchemaExporterDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JSonSchemaExporterDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b0912e3-8a3f-4fa4-8bc3-05a2d25bdd10")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
